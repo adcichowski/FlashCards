3958272becbfb5a917925dc38c0191e29fcf213a
adam.cichowski98@outlook.com <adam.cichowski98@outlook.com>
2021-06-01 17:11:09 +0200

    Add position relative to footer, problem with mobile devices

--- src/Pages/Game/Game.js
+++ src/Pages/Game/Game.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navigation from "../../Components/Navigation/Navigation";
 import styles from "./Game.module.scss";
 
 export default function Game() {
   return (
     <section className={styles.game}>
+      <Navigation />
       <div></div>
     </section>
   );
