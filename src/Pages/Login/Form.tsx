cddfefae192dbde2615647cd1518e4960b63a31a
adam.cichowski98@outlook.com <adam.cichowski98@outlook.com>
2021-08-25 10:01:43 +0200

    Change Form responsive

--- src/Pages/Login/Form.tsx
+++ src/Pages/Login/Form.tsx
@@ -17,49 +17,46 @@ function Form() {
   } = useFormLoginRegister();
   return (
     <div className={styles.game}>
-      <>
-        <div className={styles.formLog}>
-          <BackButton />
-          <h4 className={styles.formTitle}>
-            {isRegister ? "Register In" : "Log In"}
-          </h4>
-          <form className={styles.gameForm} onSubmit={handleSubmit(onSubmit)}>
-            <label>
-              <span className="sr-only">email</span>
-              <input
-                {...register("email", inputValidation.email)}
-                className={styles.inputEmail}
-                placeholder="Email:"
-                autoComplete="off"
-              />
-            </label>
-            <span className={styles.errorInfo}>{errors?.email?.message} </span>
-            <label>
-              <span className="sr-only">Password</span>
-              <input
-                {...register("password", inputValidation.password)}
-                className={styles.inputPassword}
-                placeholder="Password:"
-                type="password"
-                autoComplete="current-password"
-              />
-            </label>
-            <span className={styles.errorInfo}>
-              {errors?.password?.message}
-            </span>
+      <div className={styles.formLog}>
+        <BackButton />
+        <h4 className={styles.formTitle}>
+          {isRegister ? "Register In" : "Log In"}
+        </h4>
+        <form className={styles.gameForm} onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            <span className="sr-only">email</span>
+            <input
+              {...register("email", inputValidation.email)}
+              className={styles.inputEmail}
+              placeholder="Email:"
+              autoComplete="off"
+            />
+          </label>
+          <span className={styles.errorInfo}>{errors?.email?.message} </span>
+          <label>
+            <span className="sr-only">Password</span>
+            <input
+              {...register("password", inputValidation.password)}
+              className={styles.inputPassword}
+              placeholder="Password:"
+              type="password"
+              autoComplete="current-password"
+            />
+          </label>
+          <span className={styles.errorInfo}>{errors?.password?.message}</span>
 
-            <div className={styles.formButtons}>
-              <Button type="submit">{isRegister ? "Register" : "Login"}</Button>
-              <Button type="button" onClick={handleClickRegister}>
-                {isRegister ? "Back to login" : "Create account"}
-              </Button>
-            </div>
-            <Link to="/" className={styles.formLogo}>
-              <Logo />
-            </Link>
-          </form>
-        </div>
-      </>
+          <div className={styles.formButtons}>
+            <Button type="submit">{isRegister ? "Register" : "Login"}</Button>
+            <Button type="button" onClick={handleClickRegister}>
+              {isRegister ? "Back to login" : "Create account"}
+            </Button>
+          </div>
+          <Link to="/" className={styles.formLogo}>
+            <Logo />
+          </Link>
+        </form>
+      </div>
+
       <Modal />
     </div>
   );
