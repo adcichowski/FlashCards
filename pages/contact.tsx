d4efd578e06a6782dc8e6eec4f194c093fb9fd76
Adam Cichowski <adam.cichowski98@outlook.com>
2022-03-02 23:57:03 +0100

    add new page contanct

---
+++ pages/contact.tsx
@@ -0,0 +1,28 @@
+import styles from "../src/Pages/Contact/Contact.module.scss";
+
+export function Contact() {
+  const renderSocialLinks = socialLinks.map((socialName) => (
+    <li key={socialName}>
+      <a href={`https://${socialName}.com`}>
+        <div className={`${styles[socialName]} ${styles.social}`}>
+          <span className="sr-only">{socialName}</span>
+        </div>
+      </a>
+    </li>
+  ));
+  return (
+    <Game>
+      <Navigation />
+      <div className={styles.contact}>
+        <div className={styles.description}>
+          <h1 className={styles.descriptionTitle}>Thanks for your interest in FlashCards</h1>
+          <h2 className={styles.descriptionText}>if you wanna extend this project, message to me. </h2>
+          <ul className={styles.descriptionLinks}>{renderSocialLinks}</ul>
+        </div>
+        <GithubProfile />
+        <div className={styles.contactBackground}></div>
+      </div>
+    </Game>
+  );
+}
+Contact.displayName = "Contact";
