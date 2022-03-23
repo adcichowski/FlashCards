f5a0b5ce3ead048a28d5849abe297245512cab40
Adam Cichowski <adam.cichowski98@outlook.com>
2022-03-23 14:59:13 +0100

    feat: add loger for subjects

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
+import cors from "cors";
 import bodyParser from "body-parser";
 import express from "express";
 import { subjectRouter } from "./subject/subject.router";
@@ -8,7 +8,7 @@ const PORT = 3001;
 
 export const prisma = new PrismaClient();
 export const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", cardRouter, subjectRouter);
--- packages/server/src/subject/subject.controler.ts
+++ packages/server/src/subject/subject.controler.ts
@@ -1,5 +1,7 @@
 import { Response, Request } from "express";
+import { logger } from "../utils/logger";
 import { subjectService } from "./subject.service";
 export const getAllSubject = async (_: Request, res: Response) => {
+  logger.info("Getting all subjects from route '/subjects'");
   res.json(await subjectService.getSubjects());
 };
