4783baa1714154abefd30382ec2b95d69c1854c5
Adam Cichowski <adam.cichowski98@outlook.com>
2022-05-14 11:06:34 +0200

    feat: 🎸 config auth0 api

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,12 +2,16 @@ import BodyParser from "body-parser";
 import Cors from "cors";
 import Express from "express";
 
+import { checkJwt } from "./auth/validateAcessToken";
+import("dotenv");
 import { cardRouter } from "./card/card-router";
 import { subjectRouter } from "./subject/subject-router";
 import { routerSwagger } from "./swagger/swagger";
 import { errorHandler } from "./utils/error/errorHandler";
 import { logger } from "./utils/logger";
 import { swaggerRouter } from "./utils/swagger";
+
+import type { Request, Response } from "express";
 
 const { PORT } = process.env;
 
@@ -22,7 +26,12 @@ app.use(
   })
 );
 
-app.use(swaggerRouter, subjectRouter, cardRouter, routerSwagger);
+app.use(swaggerRouter, subjectRouter, cardRouter);
+app.get("/timesheets", checkJwt, (_: Request, res: Response) => {
+  console.log("Timesheets");
+  res.status(201).send({ message: "This is the POST /timesheets endpoint" });
+});
+app.use(routerSwagger);
 app.use(errorHandler);
 app.disable("x-powered-by");
 const server = app.listen(PORT, () => {
