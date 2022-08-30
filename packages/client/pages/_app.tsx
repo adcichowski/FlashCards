f29d5a3d56fc7508595bca5d6c9fc937cd43bab4
Adam Cichowski <adam.cichowski98@outlook.com>
2022-08-30 21:47:58 +0200

    fix: 🐛 bug fix

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,27 +1,20 @@
 import "../styles/settings.css";
 
 import type { AppProps } from "next/app";
-import { ModalProvider } from "../src/context/ModalContext";
-import { AuthProvider } from "../src/context/AuthContext";
+
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import dynamic from "next/dynamic";
 
-const ModalComponent = dynamic<{}>(() => import("src/components/Modal/Modal").then((module) => module.Modal), {
-  ssr: false,
-});
+// const ModalComponent = dynamic<{}>(() => import("../src/components/Modal/Modal").then((module) => module.Modal), {
+//   ssr: false,
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <ModalProvider>
-        <AuthProvider>
-          {process.env.NODE_ENV === "development" && <ReactQueryDevtools position="top-left" />}
-          <Component {...pageProps} />
-        </AuthProvider>
-        <ModalComponent />
-      </ModalProvider>
+      {process.env.NODE_ENV === "development" && <ReactQueryDevtools position="top-left" />}
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
